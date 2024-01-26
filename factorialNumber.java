a6d80240b9cbea1f086ed8681ac9b16d5c37a4fa
Mr Vanny <vasantbhosale28@gmail.com>
2024-01-26 21:28:39 +0530

    Most Optimised Way For Finding Factorial Of a Number

--- factorialNumber.java
+++ factorialNumber.java
@@ -1,5 +1,11 @@
 public class factorialNumber{
     public static void main(String[] args) {
         
+        int n=5;
+        int fact=1;
+        for (int i = 2; i <= 5; i++) {
+            fact*=i;
+        }
+        System.out.println("Factorial of "+n+" is "+fact);
     }
 }
